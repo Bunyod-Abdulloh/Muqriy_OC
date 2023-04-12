96f7c9dd4bd519ac7b102e59b8fa2ea8e963c0fa
Bunyod-Abdulloh <90672691+Bunyod-Abdulloh@users.noreply.github.com>
2023-04-12 09:07:42 +0500

    Yangilandi

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -6,6 +6,5 @@ async def set_default_commands(dp):
         [
             types.BotCommand("start", "Ботни қайта ишга тушириш"),
             types.BotCommand("bot_admins", "Админлар учун тугмалар"),
-            types.BotCommand("xatm_admins", "Хатм админлари учун тугмалар")
         ]
     )
